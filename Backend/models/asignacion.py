ef1b1b6460fac62f32f51c257adfbbca9d7f7029
Aldana Benavent <abenavent14389@gmail.com>
2026-04-01 19:55:49 -0300

    Add Asignacion model for database interactions

---
+++ Backend/models/asignacion.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime
+from database import Base
+
+class Asignacion(Base):
+    __tablename__ = "asignaciones"
+
+    id_asignacion = Column(Integer, primary_key=True, index=True)
+    id_solicitud = Column(Integer)
+    id_plomero = Column(Integer)
+    estado = Column(String, default="pendiente")
+    fecha_aceptacion = Column(DateTime, nullable=True)
+    fecha_completado = Column(DateTime, nullable=True)
